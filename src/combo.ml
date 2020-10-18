107791733a83f408c46a704149ac6eaac1058e56
Yul3n <yul3n.falx@protonmail.com>
2020-10-18 15:44:10 +0200

    Continued.

--- src/combo.ml
+++ src/combo.ml
@@ -5,7 +5,10 @@ type ('a, 'b) parser = 'a list -> ('b * 'a list) option
 let return a =
   fun s -> Some (a, s)
 
-let (<*>) p1 p2 =
+let fail =
+  fun _ -> None
+
+let ( <*> ) p1 p2 =
   fun s ->
   match p1 s with
     None -> None
@@ -14,19 +17,26 @@ let (<*>) p1 p2 =
        None -> None
      | Some (b, s) -> Some (a b, s)
 
-let (<|>) p1 p2 =
+let ( >>= ) p1 p2 =
+  fun s ->
+  match p1 s with
+    None -> None
+  | Some (a, s) ->
+     p2 a s
+
+let ( <|> ) p1 p2 =
   fun s ->
   match p1 s with
     None -> p2 s
   | Some _ as r -> r
 
-let (<$>) f p =
+let ( <$> ) f p =
   (return f) <*> p
 
-let (>>) p q =
+let ( *> ) p q =
   ((flip const) <$> p) <*> q
 
-let (<<) p q =
+let ( <* ) p q =
   (const <$> p) <*> q
 
 let _ = print_endline "Hello"
--- src/combo.mli
+++ src/combo.mli
@@ -1,8 +1,15 @@
 type ('a, 'b) parser = 'a list -> ('b * 'a list) option
 
 val return : 'a -> ('s, 'a) parser
-val (<*>) : ('s, 'b -> 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
-val (<|>) : ('s, 'a) parser -> ('s, 'a) parser -> ('s, 'a) parser
-val (<$>) : ('b -> 'a) -> ('s, 'b) parser -> ('s, 'a) parser
-val (>>) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'b) parser
-val (<<) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
+(** Basic parser that always fails. *)
+val fail: ('s, 'a) parser
+(** Sequence combinator appliying the result of the parser p2 to the parser p1.*)
+val ( <*> ) : ('s, 'b -> 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
+(** Sequence monad. *)
+val ( >>= ) : ('s, 'a) parser -> ('a -> ('s, 'b) parser) -> ('s, 'b) parser
+(** Choice combinator trying the parser p1, if it works, return the result, else
+    return the parser p2. *)
+val ( <|> ) : ('s, 'a) parser -> ('s, 'a) parser -> ('s, 'a) parser
+val ( <$> ) : ('b -> 'a) -> ('s, 'b) parser -> ('s, 'a) parser
+val ( *> ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'b) parser
+val ( <* ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
