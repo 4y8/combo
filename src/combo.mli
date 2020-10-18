748cc6509e56cc9738f9cc0f4f0a97ffd446572f
Yul3n <yul3n.falx@protonmail.com>
2020-10-18 15:54:38 +0200

    Continued.

--- src/combo.mli
+++ src/combo.mli
@@ -1,15 +1,29 @@
+(** Baic type of parsers. *)
 type ('a, 'b) parser = 'a list -> ('b * 'a list) option
 
+(** A basic combinator that always succeeds returning the given value. *)
 val return : 'a -> ('s, 'a) parser
-(** Basic parser that always fails. *)
+(** A basic combinator that always fails. *)
 val fail: ('s, 'a) parser
-(** Sequence combinator appliying the result of the parser p2 to the parser p1.*)
+(** Sequence combinator appliying the result of the second parser to the first
+   parser.*)
 val ( <*> ) : ('s, 'b -> 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
 (** Sequence monad. *)
 val ( >>= ) : ('s, 'a) parser -> ('a -> ('s, 'b) parser) -> ('s, 'b) parser
-(** Choice combinator trying the parser p1, if it works, return the result, else
-    return the parser p2. *)
+(** Choice combinator trying the first parser, if it works, return the result,
+   else return the result of the second parser. *)
 val ( <|> ) : ('s, 'a) parser -> ('s, 'a) parser -> ('s, 'a) parser
+(** Map combinator applying the result of the given parser to the given
+   function, if he succeeds. *)
 val ( <$> ) : ('b -> 'a) -> ('s, 'b) parser -> ('s, 'a) parser
+(** Sequence combinator ignoring the left value. *)
 val ( *> ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'b) parser
+(** Sequence combinator ignoring the right value. *)
 val ( <* ) : ('s, 'a) parser -> ('s, 'b) parser -> ('s, 'a) parser
+
+(* Basic parsers *)
+(** A parser that matches an element satisfying the given predicate. *)
+val sat : ('a -> bool) -> ('a, 'b) parser
+val char : char -> (char, char) parser
+val alpha : (char, char) parser
+val digit : (char, char) parser
